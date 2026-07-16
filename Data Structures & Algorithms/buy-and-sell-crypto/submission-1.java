e497b9190fde2d42179fefa5a079a35ec13cc582
Anmoldeep Kaur <79189406+anmoldeep-kaur03@users.noreply.github.com>
2026-07-16 23:34:47 +0530

    Update submission-1.java

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -1,13 +1,5 @@
 public class Solution {
     public int maxProfit(int[] prices) {
-        // If the two strings have different lengths, return false immediately.
-        // Create two hash maps to store character frequencies for each string.
-        // Iterate through both strings at the same time:
-        // Increase the character count for s[i] in the first map.
-        // Increase the character count for t[i] in the second map.
-        // After building both maps, compare them:
-        // If the maps are equal, return true.
-        // Otherwise, return false.
         int minBuy = prices[0]; 
         int maxP = 0;  
         for(int sell:prices){ 
